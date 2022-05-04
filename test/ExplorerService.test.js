5e04518f9a6f200e8443fd5c8533301a9467e47a
AnethArmenta <anethalvarezza@gmail.com>
2022-05-04 16:31:14 -0500

    Agregando test para Explorer service

--- test/ExplorerService.test.js
+++ test/ExplorerService.test.js
@@ -1,10 +1,19 @@
 const ExplorerService = require("./../lib/services/ExplorerService")
 describe("Test para explorer service", () => {
-    test("Requerimiento 1: Mostrar todos os explorer en mission: node", () =>{
+    test(" Mostrar todos los explorer en mission: node", () =>{
           const explorers = [{mission: "node"}]
           const explorersInNode = ExplorerService.FilterByMission(explorers, "node") 
           expect(explorersInNode.length).toBe(1)
-          
-
     });
+     test("Mostrar la cantidad de exploresrs en node", () => {
+
+        const explorers = [{mission: "node"}]   
+        const explorersInNode = ExplorerService.getAmountOfExplorersByMission(explorers,"node")
+        expect(explorersInNode).toBe(1)
+      });
+    test("Mostrar los usernames de los explorers que esten en Node", () => { 
+    const explorers = [{githubUsername: "Aneth", mission: "node"}]
+    const explorersInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node")
+    expect(explorersInNode).toBe(1)
+});
 });
