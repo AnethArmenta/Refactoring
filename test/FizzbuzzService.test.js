b3d6661bbe3d77801ade086f4190bbd83a8c6eee
AnethArmenta <anethalvarezza@gmail.com>
2022-05-04 16:50:57 -0500

    Agrenando test para FizzBuzz

---
+++ test/FizzbuzzService.test.js
@@ -0,0 +1,8 @@
+const FizzbuzzService = require("./../lib/services/FizzbuzzService")
+describe("Test para Fizzbuzz Service", () => {
+    test("Refactorizar Fizz", () =>{
+          const explorers = {name: "Woopa1", score:1}
+          const explorersInNode = FizzbuzzService.applyValidationInExplorer(explorers) 
+          expect(explorersInNode).toMatchObject({"name": "Woopa1", "score":1})
+    });
+});
